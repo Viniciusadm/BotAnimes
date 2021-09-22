3c6b0e2a1abf1cae62d8dac90afc2a8775cb3dcb
Viniciusadm <viniciusam.22@gmail.com>
2021-09-21 22:45:51 -0300

    Início do programa principal

---
+++ main.py
@@ -0,0 +1,19 @@
+from time import sleep
+from ChromeAuto import ChromeAuto
+
+chrome = ChromeAuto()
+
+audio = 'legendado'
+anime = 'detective-conan'
+start = 624
+end = 628
+
+for episode in range(start, end + 1):
+    if len(str(episode)) == 1:
+        episode = '0' + str(episode)
+    chrome.access(f'https://betteranime.net/anime/{audio}/{anime}/episodio-{episode}/download')
+    chrome.click_input_text('Qualidade HD')
+    chrome.click_by_xpath('/html/body/div/header/div/div/button')
+    chrome.back_page(0)
+    chrome.click_by_xpath('/html/body/div/header/div/div/a')
+    sleep(60)
