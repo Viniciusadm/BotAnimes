1ad14c41872b13879f05b41faea9a2481efdabe9
Viniciusadm <viniciusam.22@gmail.com>
2021-09-27 21:10:34 -0300

    Correção na classe Files

--- Files.py
+++ Files.py
@@ -5,14 +5,14 @@ class Files:
     path = '/home/vinicius/Downloads'
 
     def checkFile(self, file):
-        if os.path.exists(self.path + '/' + file):
+        if os.path.exists(f'{self.path}/{file}'):
             return True
         else: 
             return False
 
     def renameFile(self, name, episode, file):
-        sleep(5)
+        sleep(1)
         while True:
             if self.checkFile(file):
-                os.rename(file, f'{name} {episode}')
+                os.rename(f'{self.path}/{file}', f'{self.path}/{name} {episode}')
                 break
